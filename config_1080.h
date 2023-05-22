90dac5a05fe6c5661fb42daaa68e654d5b6a82cd
Daniel García Pulpeiro <danielgarciapulpeiro@gmail.com>
2023-05-22 11:09:42 +0200

    Set jetbrains font as default

--- config_1080.h
+++ config_1080.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "mono:size=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:style=Regular:size=12:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 
 static int borderpx = 2;
